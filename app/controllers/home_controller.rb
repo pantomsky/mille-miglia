d2214995de2e27e12e095d3d2e9eec2a3e5509f3
pantomsky <twitkowski05gmail.com>
2017-02-27 22:16:33 +0000

    header , bootstrap , devise

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def price
+  end
+
+  def contatc
+  end
+
+  def faq
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get price" do
+    get :price
+    assert_response :success
+  end
+
+  test "should get contatc" do
+    get :contatc
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get :faq
+    assert_response :success
+  end
+
+end
